fb281ca7088d3851abbbd55c5b4cc119938707c9
asjach <asjach@gmail.com>
2022-06-19 05:48:19 +0700

    Testing modif file

--- lib/testing_pull.dart
+++ lib/testing_pull.dart
@@ -1,3 +1,22 @@
-import 'material.dart'
-  
-  
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('title di App bar harus widget'),
+        ),
+      ),
+    );
+  }
+}
